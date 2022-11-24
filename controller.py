6e76d83ad68d27ab405bf8226249e7d023f6582f
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2022-11-24 15:25:58 +0800

    Initial Commit

---
+++ controller.py
@@ -0,0 +1,15 @@
+import model as server
+import view as view
+import socket
+
+view.open_window()
+
+
+def user_input():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return input()
+
