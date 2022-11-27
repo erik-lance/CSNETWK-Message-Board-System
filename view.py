85f4368041a46fa5b3fc59a5a293c6d39913637e
erik-lance <eriklance@gmail.com>
2022-11-28 00:44:16 +0800

    Initial GUI test

--- view.py
+++ view.py
@@ -1,9 +1,26 @@
-from tkinter import *
-
-window = Tk()
+import tkinter as tk
 
 
-def open_window():
-    window.title("Welcome to LikeGeeks app")
+
+def open_main_window():
+    window = tk.Tk()
+    window.title("Message Board System")
+    window.geometry("500x500")
+
+    label = tk.Label(text="Hello, Tkinter", background="#34A2FE")
+    label.pack()
+
+    entry = tk.Entry(fg="yellow", bg="blue", width=50)
+    entry.pack()
+
+    button = tk.Button(
+        text="Click me!",
+        width=25,
+        height=5,
+        bg="blue",
+        fg="yellow",
+    )
+    button.pack()
+
 
     window.mainloop()
