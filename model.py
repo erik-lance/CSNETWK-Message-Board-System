373f0e49a827e5eed2a5048c7b6ffaef6662e4c1
erik-lance <eriklance@gmail.com>
2022-11-27 22:53:55 +0800

    * Updated to only contain needed variables

--- model.py
+++ model.py
@@ -1,35 +1,11 @@
 import socket
 
-def test_server():
+bufferSize = 1024
 
-    localIP = "127.0.0.1"
-    localPort = 20001
-    bufferSize = 1024
+udp_host = socket.gethostname()
+udp_port = 12345
 
-    msgFromServer = "Hello UDP Client"
-    bytesToSend = str.encode(msgFromServer)
 
-    # Create a datagram socket
-    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-    # Bind to address and IP
-    UDPServerSocket.bind((localIP, localPort))
-
-    print("UDP server up and listening")
-
-    # Listen for incoming datagrams
-    while (True):
-        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-        message = bytesAddressPair[0]
-        address = bytesAddressPair[1]
-
-        client_msg = "Message from Clinet:{}".format(message)
-        client_ip = "Client IP Address:{}".format(address)
-
-        print(client_msg)
-        print(client_ip)
-
-        # Sending a reply to client
-        UDPServerSocket.sendto(bytesToSend, address)
-
-test_server()
+def get_buffer_size(): return bufferSize
+def get_udp_host(): return udp_host
+def get_udp_port(): return udp_port
