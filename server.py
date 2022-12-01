64426b62b625e3297ad5bcb77cd677d618fdaac5
erik-lance <eriklance@gmail.com>
2022-12-01 13:53:37 +0800

    Revamp from guide

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import select
 import model as model
 
 handles = []
@@ -26,6 +27,8 @@ def add_user(addr, handle=None):
     
     return msg
 
+def message_user(handle, message):
+    pass
 
 def read_command(cmd, addr):
     """ Parses the JSON input and executes command
@@ -56,33 +59,102 @@ bufferSize = model.get_buffer_size()
 udp_host = model.get_udp_host()
 udp_port = model.get_udp_port()
 
-
-
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
+UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Bind to address and IP
 UDPServerSocket.bind((udp_host, udp_port))
+UDPServerSocket.listen()
 
-print("UDP server up and listening")
+print(f"UDP server up and listening on {udp_host}:{udp_port}")
+
+socket_list = [UDPServerSocket]
+clients = {}
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recvfrom(bufferSize)
+
+        if not len(message_header):
+            return False
+
+        message = message_header.decode('UTF-8').strip()
+
+        # Object containing header and msg data
+        return {'header':message_header, 'data': client_socket.recv(len(message))}
+
+    except:
+        # Violent connection close. Can simply be closing the client or socket.close()
+        return False
 
 # Listen for incoming datagrams
 while (True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0].decode('UTF-8')
-    address = bytesAddressPair[1]
+    read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
 
-    client_msg = "Message from Client:{}".format(message)
-    client_ip = "Client IP Address:{}".format(address)
+    for notified_socket in read_sockets:
 
-    print(client_msg)
-    print(client_ip)
+        if notified_socket == UDPServerSocket:
+            client_socket, client_addr = UDPServerSocket.accept()
+
+            user = receive_message(client_socket)
+
+            # Go to next socket due to lack of header
+            if user is False:
+                continue
+
+            socket_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            client_port = user['data'].decode('utf-8')
+
+            print(f'Accepted new connection from {client_addr}:{client_port}')
+        
+        else:
+            message = receive_message(notified_socket)
+
+            if message is False:
+                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+
+                socket_list.remove(notified_socket)
+
+                del clients[notified_socket]
+
+                continue
+            
+            user = clients[notified_socket]
+
+            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            for client_socket in clients:
+
+                if client_socket != notified_socket:
+
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+
+        # Remove from list for socket.socket()
+        socket_list.remove(notified_socket)
+
+        # Remove from our list of users
+        del clients[notified_socket]
+
+    # bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    # message = bytesAddressPair[0].decode('UTF-8')
+    # address = bytesAddressPair[1]
+
+    # client_msg = "Message from Client:{}".format(message)
+    # client_ip = "Client IP Address:{}".format(address)
+
+    # print(client_msg)
+    # print(client_ip)
 
 
-    msgFromServer = read_command(message, address)
-    bytesToSend = str.encode(msgFromServer, 'UTF-8')
+    # msgFromServer = read_command(message, address)
+    # bytesToSend = str.encode(msgFromServer, 'UTF-8')
 
     #print("Message from server: "+msgFromServer)
     # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+    # UDPServerSocket.sendto(bytesToSend, address)
