843cfac0827f5256f4dfdc65f2ce78d8186eddf7
erik-lance <eriklance@gmail.com>
2022-12-04 03:18:39 +0800

    Connected GUI to client

--- view.py
+++ view.py
@@ -8,39 +8,50 @@ TEXT_COLOR = "#EAECEE"
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
+class GUI:
 
+    def __init__(self) -> None:
+        window = tk.Tk()
+        window.title("Message Board System")
+        # window.geometry("500x500")
+        app = client_app
+        app.set_gui(self)
 
-def open_main_window():
-    window = tk.Tk()
-    window.title("Message Board System")
-    # window.geometry("500x500")
-    app = client_app
+        def send():
+            send =  str(app.send_server(e.get()))
 
-    def send():
-        send =  str(app.send_server(e.get()))
+            print(send)
 
-        print(send)
+            text_board.insert(tk.END, "\n" + send)
+        
+        
+            e.delete(0, tk.END)
 
-        text_board.insert(tk.END, "\n" + send)
+        def post(message):
+            """Accessed by client object. Upon retrieval of message, tells view to post chat to board.
+
+            Args:
+                message (str): Message string to post to board.
+            """
+            print(send)
+
+            text_board.insert(tk.END, "\n" + send)
+
+        label1= tk.Label(window, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
+        row=0)
+
+        text_board = tk.Text(window, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+        text_board.grid(row=1, column=0, columnspan=2)
+
+        scrollbar = tk.Scrollbar(text_board)
+        scrollbar.place(relheight=1, relx=0.974)
+
+        e = tk.Entry(window, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
+        e.grid(row=2, column=0)
     
-    
-        e.delete(0, tk.END)
+        send = tk.Button(window, text="Send", font=FONT_BOLD, bg=BG_GRAY,
+                command=send).grid(row=2, column=1)
 
-    label1= tk.Label(window, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
-    row=0)
+        window.mainloop()
 
-    text_board = tk.Text(window, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
-    text_board.grid(row=1, column=0, columnspan=2)
-
-    scrollbar = tk.Scrollbar(text_board)
-    scrollbar.place(relheight=1, relx=0.974)
-
-    e = tk.Entry(window, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
-    e.grid(row=2, column=0)
- 
-    send = tk.Button(window, text="Send", font=FONT_BOLD, bg=BG_GRAY,
-              command=send).grid(row=2, column=1)
-
-    window.mainloop()
-
-open_main_window()
+GUI()
